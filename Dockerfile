cefdd659b21913d9529b8810d00633d2a840a8da
unknown <ocn@PC100.conscom.world>
2023-03-16 10:27:08 +0900

    bug fix

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,5 @@ WORKDIR  /usr/walmart
 
 RUN apt update \
 && apt upgrade -y \
-&& pip install --upgrade pip\
 && apt install build-essential\
 && apt install vim
---
+++ src/wanima.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "wanima.h"
+
+void clear() {
+    system("clear");
+}
+
+void main() {
+    for (int i = 0; i < 5 ;i++) {
+        usleep(400000);
+        clear();
+printf(WANIMA1);printf(WANIMA2);printf(WANIMA3);printf(WANIMA4);printf(WANIMA5);
+printf(WANIMA6);printf(WANIMA7);printf(WANIMA8);printf(WANIMA9);printf(WANIMA10);
+printf(WANIMA11);printf(WANIMA12);printf(WANIMA13);printf(WANIMA14);printf(WANIMA15);
+printf(WANIMA16);printf(WANIMA17);printf(WANIMA18);printf(WANIMA19);printf(WANIMA20);
+printf(WANIMA21);printf(WANIMA22);printf(WANIMA23);printf(WANIMA24);printf(WANIMA25);
+printf(WANIMA26);printf(WANIMA27);printf(WANIMA28);printf(WANIMA29);printf(WANIMA30);
+printf(WANIMA31);printf(WANIMA32);printf(WANIMA33);printf(WANIMA34);printf(WANIMA35);
+printf(WANIMA36);printf(WANIMA37);printf(WANIMA38);printf(WANIMA39);printf(WANIMA40);
+printf(WANIMA41);
+        usleep(400000);
+        clear();
+printf(OTAKU1);printf(OTAKU2);printf(OTAKU3);printf(OTAKU4);printf(OTAKU5);
+printf(OTAKU6);printf(OTAKU7);printf(OTAKU8);printf(OTAKU9);printf(OTAKU10);
+printf(OTAKU11);printf(OTAKU12);printf(OTAKU13);printf(OTAKU14);printf(OTAKU15);
+printf(OTAKU16);printf(OTAKU17);printf(OTAKU18);printf(OTAKU19);printf(OTAKU20);
+printf(OTAKU21);printf(OTAKU22);printf(OTAKU23);printf(OTAKU24);printf(OTAKU25);
+printf(OTAKU26);printf(OTAKU27);printf(OTAKU28);printf(OTAKU29);printf(OTAKU30);
+printf(OTAKU31);printf(OTAKU32);printf(OTAKU33);printf(OTAKU34);printf(OTAKU35);
+printf(OTAKU36);printf(OTAKU37);printf(OTAKU38);printf(OTAKU39);printf(OTAKU40);
+printf(OTAKU41);
+        usleep(400000);
+        clear();
+}
+    
+    // 最後に笑顔を表示
+    clear();
+printf(WANIOTA1);printf(WANIOTA2);printf(WANIOTA3);printf(WANIOTA4);printf(WANIOTA5);
+printf(WANIOTA6);printf(WANIOTA7);printf(WANIOTA8);printf(WANIOTA9);printf(WANIOTA10);
+printf(WANIOTA11);printf(WANIOTA12);printf(WANIOTA13);printf(WANIOTA14);printf(WANIOTA15);
+printf(WANIOTA16);printf(WANIOTA17);printf(WANIOTA18);printf(WANIOTA19);printf(WANIOTA20);
+printf(WANIOTA21);printf(WANIOTA22);printf(WANIOTA23);printf(WANIOTA24);printf(WANIOTA25);
+printf(WANIOTA26);printf(WANIOTA27);printf(WANIOTA28);printf(WANIOTA29);printf(WANIOTA30);
+printf(WANIOTA31);printf(WANIOTA32);printf(WANIOTA33);printf(WANIOTA34);printf(WANIOTA35);
+printf(WANIOTA36);printf(WANIOTA37);printf(WANIOTA38);printf(WANIOTA39);printf(WANIOTA40);
+printf(WANIOTA41);
+    exit(0);
+}
